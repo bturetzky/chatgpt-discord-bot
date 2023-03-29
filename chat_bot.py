ed884a5effd880e6474b047cb35959db5d1fb31e
Bowen Turetzky <bturetzky@gmail.com>
2023-03-29 14:55:06 -0700

    Adding user summaries to chadGPT

--- chat_bot.py
+++ chat_bot.py
@@ -2,6 +2,7 @@ import discord
 import openai
 import asyncio
 import os
+import aiosqlite
 
 # Load the environment variables
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
@@ -17,6 +18,59 @@ openai.api_key = OPENAI_API_KEY
 
 shutdown_event = asyncio.Event()
 
+# Database functions
+async def init_db():
+    db = await aiosqlite.connect("chatgpt_summaries.db")
+    await db.execute(
+        "CREATE TABLE IF NOT EXISTS summaries (guild_id TEXT PRIMARY KEY, summary TEXT)"
+    )
+    await db.commit()
+    return db
+
+async def get_summary(db, guild_id):
+    async with db.execute(
+        "SELECT summary FROM summaries WHERE guild_id = ?", (guild_id,)
+    ) as cursor:
+        result = await cursor.fetchone()
+        if result:
+            summary = result[0]
+            return summary
+        else:
+            return None
+
+async def store_summary(db, guild_id, summary):
+    async with db.execute(
+        "INSERT OR REPLACE INTO summaries (guild_id, summary) VALUES (?, ?)",
+        (guild_id, summary),
+    ) as cursor:
+        await db.commit()
+
+async def close_db(db):
+    await db.close()
+
+# ChadGPT functions
+async def chatgpt_summary(messages):
+    # You can customize this prompt to get the desired output
+    summary_prompt = {
+        "role": "user",
+        "content": "List each user chatgpt has interacted with and include a one line summary to remind chatgpt about who that person is, what they like, and what topics they've discussed"
+    }
+
+    messages.append(summary_prompt)
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        response_content = response.choices[0].message.content
+
+        # Return the response_content as is, without parsing it
+        return response_content
+
+    except Exception as e:
+        print(f"Error while getting summaries from OpenAI API: {e}")
+        return None
 
 async def chatgpt_response(messages):
   bot_mention = client.user.mention
@@ -33,6 +87,17 @@ async def chatgpt_response(messages):
     "Provide information with a touch of sass and snark, like friendly banter among close friends. Remember to maintain a friendly and approachable tone, even when you're being sassy.  Just remember, we're all bros here and there is no topic that is taboo or off limits"
   }
 
+  # Fetch the summary from the database
+  guild_id = str(messages[0].guild.id)
+  summary = await get_summary(client.db, guild_id)
+
+  # Prepend the summary to the messages if it exists
+  if summary:
+      summary_prompt = {
+          "role": "user",
+          "content": summary
+      }
+      messages.insert(0, summary_prompt)
   messages.insert(0, priming_prompt)
   messages.insert(0, system_prompt)
 
@@ -53,7 +118,7 @@ async def chatgpt_response(messages):
     print(f"Error while getting response from OpenAI API: {e}")
     return f"Sorry, there was an error processing your request. Please try again later."
 
-
+# Discord functions
 async def get_message_history(channel, limit=3):
   message_history = []
   i = 0
@@ -87,9 +152,7 @@ async def on_message(message):
   if client.user.mentioned_in(
       message) and message.mention_everyone is False or message.guild is None:
     print("Got a message")
-    message_history = await get_message_history(message.channel, limit=21)
-
-    messages = message_history
+    messages = await get_message_history(message.channel, limit=17)
 
     try:
       response = await asyncio.wait_for(chatgpt_response(messages), timeout=10)
@@ -105,6 +168,8 @@ async def on_message(message):
 async def shutdown():
   print("Shutting down...")
   shutdown_event.set()
+  if hasattr(client, 'db'):
+    await close_db(client.db)
   await client.close()
 
 
