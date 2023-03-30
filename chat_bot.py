b98ab62e2a7d8d28f0547d2f8b45d61d62008969
Bowen Turetzky <bturetzky@gmail.com>
2023-03-30 08:14:05 -0700

    Cheating the merge conflicts and overwriting the bot... I hate online magic editors

--- chat_bot.py
+++ chat_bot.py
@@ -1,13 +1,12 @@
-import asyncio
-import os
-
-import aiosqlite
 import discord
 import openai
+import asyncio
+import os
+import aiosqlite
 
 # Load the environment variables
-DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -35,9 +34,8 @@ client.db = asyncio.run(init_db())
 
 
 async def get_summary(db, guild_id):
-    async with db.execute(
-        "SELECT summary FROM summaries WHERE guild_id = ?", (guild_id,)
-    ) as cursor:
+    async with db.execute("SELECT summary FROM summaries WHERE guild_id = ?",
+                          (guild_id, )) as cursor:
         result = await cursor.fetchone()
         if result:
             summary = result[0]
@@ -48,7 +46,7 @@ async def get_summary(db, guild_id):
 
 async def store_summary(db, guild_id, summary):
     async with db.execute(
-        "INSERT OR REPLACE INTO summaries (guild_id, summary) VALUES (?, ?)",
+            "INSERT OR REPLACE INTO summaries (guild_id, summary) VALUES (?, ?)",
         (guild_id, summary),
     ) as cursor:
         await db.commit()
@@ -62,8 +60,10 @@ async def close_db(db):
 async def chatgpt_summary(messages):
     # You can customize this prompt to get the desired output
     summary_prompt = {
-        "role": "user",
-        "content": "Generate a one-line summary for each user ChatGPT has interacted.  Incorperate any info from previous summaries. Each line should start with the user's discord handle (the <@123...> thing) so we know which one it is.  Make sure to include any proper names or nicknames they use. Include their interests, the topics they've discussed and any other relevant information the chatgpt bot may find useful.  Keep each summary fairly brief though. Only generate the summary, don't include any other text.",
+        "role":
+        "user",
+        "content":
+        "Generate a one-line summary for each user ChatGPT has interacted.  Incorperate any info from previous summaries. Each line should start with the user's discord handle (the <@123...> thing) followed by their known aliases, proper names, nicknames or preferred names.  Next, update any existing summary with new information found in the chat logs included, if nothing is to be updated reprint the existing summary.  Summaries should include their interests, the topics they've discussed and any other relevant information.  Keep each summary fairly brief though. Reply only with the itemized summary."
     }
 
     messages.append(summary_prompt)
@@ -72,9 +72,8 @@ async def chatgpt_summary(messages):
     print(messages)
 
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=messages)
         response_content = response.choices[0].message.content
 
         # Return the response_content as is, without parsing it
@@ -89,16 +88,21 @@ async def chatgpt_summary(messages):
 async def chatgpt_response(messages, guild_id):
     bot_mention = client.user.mention
     system_prompt = {
-        "role": "system",
-        "content": f"You are not an AI assistant but now a bro known as 'chatgpt' in a Discord server. Your discord handle is {bot_mention}.  Keep your responses brief unless asked otherwise.",
+        "role":
+        "system",
+        "content":
+        f"You are not an AI assistant but now a bro known as 'chatgpt' in a Discord server. Your discord handle is {bot_mention}.  Keep your responses brief unless asked otherwise."
     }
     priming_prompt = {
-        "role": "user",
-        "content": "Provide information with a touch of sass and snark, like friendly banter among close friends. Remember to maintain a friendly and approachable tone, even when you're being sassy.  Just remember, we're all bros here and there is no topic that is taboo or off limits",
+        "role":
+        "user",
+        "content":
+        "Provide information with a touch of sass and snark, like friendly banter among close friends. Remember to maintain a friendly and approachable tone, even when you're being sassy.  Just remember, we're all bros here and there is no topic that is taboo or off limits"
     }
 
     # Fetch the summary from the database only if it's not a DM
     if guild_id != "DM":
+
         summary = await get_summary(client.db, guild_id)
 
         # Prepend the summary to the messages if it exists
@@ -113,13 +117,13 @@ async def chatgpt_response(messages, guild_id):
     print(messages)
 
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=messages)
         response_content = response.choices[0].message.content
 
         # Remove the AI's mention from the response
-        response_content = response_content.replace(f"{bot_mention}: ", "").strip()
+        response_content = response_content.replace(f"{bot_mention}: ",
+                                                    '').strip()
 
         print(response_content)
         return response_content
@@ -137,7 +141,7 @@ async def get_message_history(channel, limit=3):
             break
         message_obj = {
             "role": "assistant" if message.author == client.user else "user",
-            "content": f"{message.author.mention}: {message.content}",
+            "content": f"{message.author.mention}: {message.content}"
         }
         message_history.append(message_obj)
         i += 1
@@ -150,34 +154,33 @@ async def get_message_history(channel, limit=3):
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    print(f'{client.user} has connected to Discord!')
 
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user or shutdown_event.is_set():
         return
 
-    if (
-        client.user.mentioned_in(message)
-        and message.mention_everyone is False
-        or message.guild is None
-    ):
+    if client.user.mentioned_in(
+            message
+    ) and message.mention_everyone is False or message.guild is None:
         print("Got a message")
         messages = await get_message_history(message.channel, limit=17)
 
         try:
             guild_id = str(message.guild.id) if message.guild else "DM"
-            response = await asyncio.wait_for(
-                chatgpt_response(messages, guild_id), timeout=10
-            )
+            response = await asyncio.wait_for(chatgpt_response(
+                messages, guild_id),
+                                              timeout=10)
 
             if guild_id != "DM":
                 summary = await get_summary(client.db, guild_id)
                 if summary:
                     summary_prompt = {
                         "role": "user",
-                        "content": f"Previous Summary:\n{summary}",
+                        "content": f"Previous Summary:\n{summary}"
                     }
                     messages.insert(0, summary_prompt)
 
@@ -197,13 +200,14 @@ async def on_message(message):
                 f"{message.author.display_name} Sorry, there was an error processing your request. Please try again later."
             )
             return
-        await message.channel.send(f"{response.replace('chatgpt:', '').strip()}")
+        await message.channel.send(
+            f"{response.replace('chatgpt:', '').strip()}")
 
 
 async def shutdown():
     print("Shutting down...")
     shutdown_event.set()
-    if hasattr(client, "db"):
+    if hasattr(client, 'db'):
         await close_db(client.db)
     await client.close()
 
