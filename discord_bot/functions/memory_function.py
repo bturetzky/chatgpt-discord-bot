c6d9e94e79cb24ac555d349adef0c1f624414b03
Bowen Turetzky <bturetzky@gmail.com>
2023-10-07 12:29:54 -0700

    Adding more description to memory function

--- discord_bot/functions/memory_function.py
+++ discord_bot/functions/memory_function.py
@@ -3,13 +3,13 @@ from discord_bot.handlers.vector_handler import VectorHandler
 
 class WeatherLookup(BaseFunction):
     name = "store_memory"
-    description = "Stores important or information-rich details into memories. Use this function to save key details or insights that should be remembered for future interactions."
+    description = "Stores important or information-rich details into memories. Use this function to save key details or insights that should be remembered for future interactions. Prioritize storing memories that help the bot understand individual users and their preferences for more personalized future interactions."
     parameters = {
         "type": "object",
         "properties": {
             "memory": {
                 "type": "string",
-                "description": "The specific memory to store. This should be a concise yet detailed summary of important information or insights that would be beneficial for future interactions."
+                "description": "The specific memory to store. This should be a concise yet detailed summary of important information or insights. Prioritize storing memories that help the bot understand individual users and their preferences for more personalized future interactions."
             }
         },
         "required": ["memory"]
